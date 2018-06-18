33f9da482b02e0d2d1d5729f0e0376d0cd5ddd5b
Justin <justin@pointw.com>
2018-06-18 12:46:51 -0500

    Got basic property mixins working

--- src/Core/Traits/Mixable.php
+++ src/Core/Traits/Mixable.php
@@ -3,6 +3,8 @@
 namespace ElephantWrench\Core\Traits;
 
 use Closure;
+use ReflectionClass;
+use ReflectionProperty;
 use BadMethodCallException;
 
 trait Mixable
@@ -12,7 +14,10 @@ trait Mixable
      *
      * @var array
      */
-    protected static $mixables = array();
+    protected static $mixable_methods = array();
+
+
+    protected static $mixable_properties = array();
 
     /**
      * Register a new function to this class
@@ -22,7 +27,52 @@ trait Mixable
      */
     public static function mix(string $name, callable $macro)
     {
-        static::$mixables[static::class][$name] = $macro;
+        static::$mixable_methods[static::class][$name] = $macro;
+    }
+
+    // protected static function mix_property($property, $value)
+    // {
+    //     static::$mixable_properties[static::class][$property] = $value;
+    // }
+
+    protected static function mix_property(ReflectionProperty $property)
+    {
+        static::$mixable_properties[static::class][$property->getName()] = $property;
+    }
+
+    public static function getMixedPropertyClass(string $property, bool $private_property = False)
+    {
+        $class = static::class;
+        while ($class !== False) {
+            if (isset(static::$mixable_properties[$class][$property])) {
+                break;
+            }
+            // If private_property is True set class to False to prevent checking parent classes
+            $class = $private_property ? False : get_parent_class($class);
+        }
+        return $class;
+    }
+
+    /**
+     * Register all properties and functions of another class to this one
+     *
+     * @param  mixed $class Fully qualified name of a class or an instance of one
+     */
+    public static function mixin($mixin)
+    {
+        $reflection_class = new ReflectionClass($mixin);
+        //foreach($reflection_class->getDefaultProperties() as $property => $value)
+        //{
+        //    static::mix_property($property, $value);
+        //}
+        $default_property_values = $reflection_class->getDefaultProperties();
+        foreach ($reflection_class->getProperties() as $property)
+        {
+            if ($property->isDefault()) {
+                $property->mixin_value = $default_property_values[$property->getName()];
+                static::mix_property($property);
+            }
+        }
     }
 
     /**
@@ -32,11 +82,11 @@ trait Mixable
      *
      * @return string|False
      */
-    public static function getMixedClass(string $method)
+    public static function getMixedMethodClass(string $method)
     {
         $class = static::class;
         while ($class !== False) {
-            if (isset(static::$mixables[$class][$method])) {
+            if (isset(static::$mixable_methods[$class][$method])) {
                 break;
             }
             $class = get_parent_class($class);
@@ -51,9 +101,9 @@ trait Mixable
      *
      * @return boolean
      */
-    public static function hasMixedFunction(string $method) : bool
+    public static function hasMixedMethod(string $method) : bool
     {
-        return (bool) static::getMixedClass($method);
+        return (bool) static::getMixedMethodClass($method);
     }
 
     /**
@@ -68,16 +118,31 @@ trait Mixable
      */
     public function __call(string $method, array $parameters)
     {
-        $mixed_class = static::getMixedClass($method);
+        $mixed_class = static::getMixedMethodClass($method);
         if (!$mixed_class) {
             throw new BadMethodCallException(sprintf(
                 'Method %s::%s does not exist.', static::class, $method
             ));
         }
 
-        $callable = static::$mixables[$mixed_class][$method];
+        $callable = static::$mixable_methods[$mixed_class][$method];
         $callable = Closure::bind($callable, $this, $mixed_class);
 
         return call_user_func_array($callable, $parameters);
+    }
+
+    public function __get(string $name)
+    {
+        $mixed_class = static::getMixedPropertyClass($name);
+        if ($mixed_class) {
+            $property = static::$mixable_properties[$mixed_class][$name];
+            if ($property->isPublic()) {
+                return $property->mixin_value;
+            }
+        }
+
+        trigger_error(sprintf(
+            'Undefined Property: %s::%s', static::class, $name
+        ));
     }
 }
